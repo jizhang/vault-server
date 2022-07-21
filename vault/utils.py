3040244f16caf29dddbe6bd706658bf9db0bdbc0
Ji ZHANG <zhangji87@gmail.com>
2022-07-21 08:58:54 +0800

    Add rows_to_list util

--- vault/utils.py
+++ vault/utils.py
@@ -1,4 +1,10 @@
 import pkgutil
+from typing import Iterable, List
+from decimal import Decimal
+from datetime import datetime
+
+from sqlalchemy.engine.row import Row
+from sqlalchemy.orm.decl_api import DeclarativeMeta
 
 
 def load_module_recursively(module) -> None:
@@ -9,3 +15,24 @@ def load_module_recursively(module) -> None:
 
         if ispkg:
             load_module_recursively(_module)
+
+
+def row_to_dict(row) -> dict:
+    if isinstance(row, Row):
+        row_dict = dict(row)
+    elif isinstance(row.__class__, DeclarativeMeta):
+        row_dict = {c.name: getattr(row, c.name) for c in row.__table__.columns}
+    else:
+        raise ValueError('Unsupported row type.')
+
+    for key, value in row_dict.items():
+        if isinstance(value, Decimal):
+            row_dict[key] = float(value)
+        elif isinstance(value, datetime):
+            row_dict[key] = value.strftime('%Y-%m-%d %H:%M:%S')
+
+    return row_dict
+
+
+def rows_to_list(rows: Iterable) -> List[dict]:
+    return [row_to_dict(i) for i in rows]
